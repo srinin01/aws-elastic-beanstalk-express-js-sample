2a5af6f31841b705a9049579e61992863c856122
srinin01 <58861911+srinin01@users.noreply.github.com>
2021-05-19 17:59:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello SRINi!'));
+app.get('/', (req, res) => res.send('Hello Amazon!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
